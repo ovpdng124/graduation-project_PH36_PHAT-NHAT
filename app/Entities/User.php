eb8a2a4d8ab708a00ce7b6d31452fa488fbf467b
ovpdng124 <ongvanphat124@gmail.com>
2020-02-06 01:00:40 +0700

    Fix authentication

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -2,10 +2,10 @@
 
 namespace App\Entities;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
     use SoftDeletes;
 
